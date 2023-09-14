e67a284c4b8284ae9ae984a08108ea4da0c4a07d
mrunmayee17 <mrunmayeerane17@gmail.com>
2023-09-14 07:37:50 -0400

    testing3

--- utility/load_dataset.py
+++ utility/load_dataset.py
@@ -1,21 +1,21 @@
 import pickle
 
-# def load_data_train(folder_name):
-#     with open('german-traffic-signs/train.p', 'rb') as f:
-#     train_data = pickle.load(f)
-#     X_train, y_train = train_data['features'], train_data['labels']
-#     return X_train, y_train
+def load_train_data(folder_name):
+    with open(folder_name, 'rb') as f:
+        train_data = pickle.load(f)
+    X_train, y_train = train_data['features'], train_data['labels']
+    return X_train, y_train
 
-# def load_validation_data(folder_name):
-#     with open('german-traffic-signs/valid.p', 'rb') as f:
-#     val_data = pickle.load(f)
-#     X_val, y_val = val_data['features'], val_data['labels']
-#     return X_val, y_val
+def load_validation_data(folder_name):
+    with open(folder_name, 'rb') as f:
+        val_data = pickle.load(f)
+    X_val, y_val = val_data['features'], val_data['labels']
+    return X_val, y_val
 
-# def load_test_data(folder_name):
-#     with open('german-traffic-signs/test.p', 'rb') as f:
-#     test_data = pickle.load(f)
-#     X_test, y_test = test_data['features'], val_data['labels']
-#     return X_test, y_test
+def load_test_data(folder_name):
+    with open(folder_name, 'rb') as f:
+        test_data = pickle.load(f)
+    X_test, y_test = test_data['features'], test_data['labels']
+    return X_test, y_test
     
 
